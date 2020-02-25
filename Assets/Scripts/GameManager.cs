bf95815ab277180a838d70724042b8a2d965e417
Romina Marsico <rominamarsico@users.noreply.github.com>
2020-02-25 15:49:24 +0100

    added object prefabs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,10 +9,10 @@ public class GameManager : MonoBehaviour
 
     public class MyObjetcs 
     {
-        public bool cube;
+        public bool cake;
         public bool donut;
-        public bool rectangle;
-        public bool sphere;
+        public bool hamburger;
+        public bool icecream;
     }
 
     // Start is called before the first frame update
@@ -25,7 +25,6 @@ public class GameManager : MonoBehaviour
     {
         UnityWebRequest getFirestoreData = new UnityWebRequest("https://us-central1-techtalk-demo-6ecbb.cloudfunctions.net/readFromFirestore");
         getFirestoreData.downloadHandler = new DownloadHandlerBuffer();
-        getFirestoreData.chunkedTransfer = false;
         yield return getFirestoreData.SendWebRequest();
 
         string firestoreData = getFirestoreData.downloadHandler.text;
@@ -33,7 +32,7 @@ public class GameManager : MonoBehaviour
         if(getFirestoreData.isNetworkError || getFirestoreData.isHttpError) 
         {
             Debug.Log(getFirestoreData.error);
-        } else {
+        } else if(firestoreData != null){
             Debug.Log(firestoreData);
             CreateFromJSON(firestoreData);
         }
@@ -41,13 +40,15 @@ public class GameManager : MonoBehaviour
 
     void CreateFromJSON(string firestoreData)
     {
-        bool cubeValue = JsonUtility.FromJson<MyObjetcs>(firestoreData).cube;
+        bool cakeValue = JsonUtility.FromJson<MyObjetcs>(firestoreData).cake;
         bool donutValue = JsonUtility.FromJson<MyObjetcs>(firestoreData).donut;
-        bool rectangleValue = JsonUtility.FromJson<MyObjetcs>(firestoreData).rectangle;
-        bool sphereValue = JsonUtility.FromJson<MyObjetcs>(firestoreData).sphere;
+        bool hamburgerValue = JsonUtility.FromJson<MyObjetcs>(firestoreData).hamburger;
+        bool icecreamValue = JsonUtility.FromJson<MyObjetcs>(firestoreData).icecream;
 
-        ShowObjects(cubeValue, "Cube");
-        ShowObjects(sphereValue, "Sphere");
+        ShowObjects(cakeValue, "Cake");
+        ShowObjects(donutValue, "Donuts");
+        ShowObjects(hamburgerValue, "Hamburger");
+        ShowObjects(icecreamValue, "IceCream");
     }
 
     void ShowObjects(bool showObject, string objectName)
